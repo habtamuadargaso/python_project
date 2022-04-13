ec5e173b43bc2df9cc7beae43e4aaeea24e7dfbf
habtamuadargaso <habtamu.adargaso@gmail.com>
2022-04-13 10:44:27 -0700

    project

--- QR_python/qr_code/qrcodeFile/QR_Code_github.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-data = "https://github.com/habtamuadargaso/Java/commit/bfba65a863230ab40236f4e1acfe79680b2416b5"
-QRCodefile = "My_githube_File.png"
-QRimage =qrcode.make(data)
-QRimage.save(QRCodefile)
